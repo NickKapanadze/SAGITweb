292579294f92182ca8f1e21decfe955e0c2ab445
Nika kapanadze <n.kapanadze@self.ge>
2025-12-14 19:44:05 +0400

    revert "Merge pull request #2 from NickKapanadze/copilot/create-mafia-game-platform"
    This reverts commit 808440cd644dc6ef2aa4ac0cd3d7c3d0ccedb1e0, reversing
    changes made to e96bfebabf828e27555502394bd8264cfb8789f5.

--- backend/src/entities/index.ts
+++
@@ -1,6 +0,0 @@
-export { Session } from './session.entity';
-export { Player } from './player.entity';
-export { Role } from './role.entity';
-export { Vote } from './vote.entity';
-export { NightAction } from './night-action.entity';
-export { Foul } from './foul.entity';
